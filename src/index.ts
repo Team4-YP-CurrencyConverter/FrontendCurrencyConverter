b33899e03a3ed71ef02d06c870357c6b170cb1d3
Alexey Efimov <alex-vngd@yandex.ru>
2024-12-17 12:55:14 +0300

    верстка 2х инпутов для валют

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
 import './index.css';
-import CurrencyButton from './components/button/button.ts';
-import data from './data/fake.ts';
-
-const btn = new CurrencyButton('#card', data);
-btn.createButton();
