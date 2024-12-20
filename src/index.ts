fab3bfd68f0afa1eafda3df6e6966a85a0a85b6b
Alexey Efimov <alex-vngd@yandex.ru>
2024-12-20 12:16:19 +0300

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import './index.css';
 import getCurrencies from './utils/api/currenciesApi.ts';
-import handlePopup from './utils/handlers/popup.ts';
 import handleSelectButton from './utils/handlers/selectbutton.ts';
 
 const data = await getCurrencies();
 // eslint-disable-next-line no-console
 console.log(data);
 handleSelectButton();
-handlePopup();
