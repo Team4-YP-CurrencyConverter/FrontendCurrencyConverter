98b9706fea02a3e1c4ca1762c3539ec3b4c7539a
Alexey Efimov <alex-vngd@yandex.ru>
2024-12-22 22:47:48 +0300

    fixes: поиск валюты для всех попапов

--- src/utils/handlers/searchcurrency.ts
+++ src/utils/handlers/searchcurrency.ts
@@ -1,24 +1,26 @@
 /* eslint-disable no-undef */
 const handleSearchCurrency = () => {
-  const input = document.querySelector('.popup__input');
-  const image = document.querySelector('.popup__search-img');
-  input?.addEventListener('input', () => {
-    if ((<HTMLInputElement>input).value !== '') {
-      (<HTMLImageElement>image).src = './src/assets/input-close.svg';
-    } else {
-      (<HTMLImageElement>image).src = './src/assets/input-search.svg';
-    }
-    const currenciesTags = input?.closest('.popup')?.querySelectorAll('.currencycard__name');
-    const currencies: (string | null)[] = [];
-    currenciesTags?.forEach((item) => {
-      currencies.push(item.textContent);
-      if (!item.textContent?.includes((<HTMLInputElement>input).value)) {
-        item.closest('.currencycard')?.classList.add('currencycard__hidden');
-        item.closest('.currencycard')?.classList.remove('currencycard__visible');
+  const inputs = document.querySelectorAll('.popup__input');
+  inputs.forEach((input) => {
+    const image = input.closest('.popup__search')!.querySelector('.popup__search-img');
+    input?.addEventListener('input', () => {
+      if ((<HTMLInputElement>input).value !== '') {
+        (<HTMLImageElement>image).src = './src/assets/input-close.svg';
       } else {
-        item.closest('.currencycard')?.classList.remove('currencycard__hidden');
-        item.closest('.currencycard')?.classList.add('currencycard__visible');
+        (<HTMLImageElement>image).src = './src/assets/input-search.svg';
       }
+      const currenciesTags = input?.closest('.popup')?.querySelectorAll('.currencycard__name');
+      const currencies: (string | null)[] = [];
+      currenciesTags?.forEach((item) => {
+        currencies.push(item.textContent);
+        if (!item.textContent?.includes((<HTMLInputElement>input).value)) {
+          item.closest('.currencycard')?.classList.add('currencycard__hidden');
+          item.closest('.currencycard')?.classList.remove('currencycard__visible');
+        } else {
+          item.closest('.currencycard')?.classList.remove('currencycard__hidden');
+          item.closest('.currencycard')?.classList.add('currencycard__visible');
+        }
+      });
     });
   });
 };
