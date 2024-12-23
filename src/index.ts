a5c7a9a5290aabf3221a386cd458730834bc0494
VeselovTimofey <vestimofey@mail.ru>
2024-12-23 21:39:10 +0300

    delete: api request in index.ts && processed_currencies in db.json

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,8 @@
-/* eslint-disable no-console */
 import './index.css';
-import getConversionAmount from './utils/api/conversionApi.ts';
-import getCurrencies from './utils/api/currenciesApi.ts';
 import handleAddButton from './utils/handlers/addbutton.ts';
 import handleToggleButton from './utils/handlers/closeinputbutton.ts';
 import handleSelectButton from './utils/handlers/selectbutton.ts';
 
-// todo: delete all api fetch
-const currenciesData = await getCurrencies();
-console.log(currenciesData);
-const firstConversionData = await getConversionAmount(1000, 'RUBUSD');
-console.log(firstConversionData);
-const secondConversionData = await getConversionAmount(5, 'EURRUBTRY');
-console.log(secondConversionData);
-const thirdConversionData = await getConversionAmount(10, 'GBPEURRUBCNY');
-console.log(thirdConversionData);
 handleSelectButton();
 handleAddButton();
 handleToggleButton();
