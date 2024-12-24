9636b076eb257afcca0574aa735a9a7b455cfcc5
VeselovTimofey <vestimofey@mail.ru>
2024-12-24 03:26:39 +0300

    feat: bind getCurrencies with currencyCard

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,14 @@
 import './index.css';
+import mainInitialization from './render/mainInitialization.ts';
 import handleAddButton from './utils/handlers/addbutton.ts';
 import handleToggleButton from './utils/handlers/closeinputbutton.ts';
 import handleSearchCurrency from './utils/handlers/searchcurrency.ts';
 import handleSelectButton from './utils/handlers/selectbutton.ts';
-import fakedata from './data/fake.ts';
-import currencyCardRenderer, { cardContainers } from './utils/api/currencycardrenderer.ts';
-import handleCurrencyButton from './utils/handlers/currencybutton.ts';
 
 handleSelectButton();
 handleAddButton();
 handleToggleButton();
 
-fakedata.forEach((item) => {
-  currencyCardRenderer(item, cardContainers[0]);
-});
-fakedata.forEach((item) => {
-  currencyCardRenderer(item, cardContainers[1]);
-});
-fakedata.forEach((item) => {
-  currencyCardRenderer(item, cardContainers[2]);
-});
-fakedata.forEach((item) => {
-  currencyCardRenderer(item, cardContainers[3]);
-});
+await mainInitialization();
 
 handleSearchCurrency();
-
-handleCurrencyButton(fakedata);
