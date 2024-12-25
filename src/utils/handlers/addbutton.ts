39214e147564a2fa4890045b837c99062543e748
VeselovTimofey <vestimofey@mail.ru>
2024-12-25 11:12:34 +0300

    feat: add conversion of appered currency

--- src/utils/handlers/addbutton.ts
+++ src/utils/handlers/addbutton.ts
@@ -1,31 +1,50 @@
+import getConversionAmount from '../api/conversionApi.ts';
+
 /* eslint-disable no-undef */
 const handleAddButton = () => {
   const addButton = document.querySelector('.button');
 
   addButton?.addEventListener('click', () => {
-    /* ↓↓ появление - скрытие кнопок удаления инпута ↓↓ */
-    const inputs = document.querySelectorAll('.converter__input');
-    const toogleButtons = document.querySelectorAll('.converter__input-toogle');
-    if (inputs.length > 2) {
-      toogleButtons.forEach((item) => {
-        item.classList.remove('converter__input-toogle-hidden');
-      });
-    } else if (inputs.length < 3) {
-      toogleButtons.forEach((item) => {
-        item.classList.add('converter__input-toogle-hidden');
-      });
-    }
-    /* ↑↑ появление - скрытие кнопок удаления инпута ↑↑ */
-    const input = document.querySelector('.converter__input_hidden');
-    input?.classList.remove('converter__input_hidden');
-    const inp = document.querySelector('.converter__input_hidden');
-    if (!inp) {
-      addButton?.classList.add('button__hidden');
-      addButton.classList.remove('button__visible');
-    } else {
-      addButton?.classList.add('button__visible');
-      addButton.classList.remove('button__hidden');
-    }
+    // eslint-disable-next-line no-void
+    void (async () => {
+      /* ↓↓ появление - скрытие кнопок удаления инпута ↓↓ */
+      const inputs = document.querySelectorAll('.converter__input');
+      // This is the first non-hidden currency in converter
+      const referenceCurrencyObject = document.querySelector('.converter__wrapper-input-toogle:not(.converter__input_hidden)') as HTMLElement;
+      const toogleButtons = document.querySelectorAll('.converter__input-toogle');
+      if (inputs.length > 2) {
+        toogleButtons.forEach((item) => {
+          item.classList.remove('converter__input-toogle-hidden');
+        });
+      } else if (inputs.length < 3) {
+        toogleButtons.forEach((item) => {
+          item.classList.add('converter__input-toogle-hidden');
+        });
+      }
+      /* ↑↑ появление - скрытие кнопок удаления инпута ↑↑ */
+      const input = document.querySelector('.converter__input_hidden');
+      input?.classList.remove('converter__input_hidden');
+      const inp = document.querySelector('.converter__input_hidden');
+      if (!inp) {
+        addButton?.classList.add('button__hidden');
+        addButton.classList.remove('button__visible');
+      } else {
+        addButton?.classList.add('button__visible');
+        addButton.classList.remove('button__hidden');
+      }
+
+      const selectedCurrencyName = input?.querySelector('.select__text')?.innerHTML as string;
+      const referenceCurrencyAmount = (referenceCurrencyObject?.querySelector('.converter__textinput') as HTMLInputElement).value;
+      const referenceCurrencyName = referenceCurrencyObject?.querySelector('.select__text')?.innerHTML;
+
+      const selectedCurrencyAmount = await getConversionAmount(
+        Number(referenceCurrencyAmount),
+        referenceCurrencyName + selectedCurrencyName,
+      );
+
+      const selectedCurrencyInput = input?.querySelector('.converter__textinput') as HTMLInputElement;
+      selectedCurrencyInput.value = selectedCurrencyAmount[0].toString();
+    })();
   });
 };
 
